38ad2d7226962e20a02bd530bafbe867879e9186
KKKYW <hopan678@gmail.com>
2022-09-20 17:55:21 +0900

    2022-09-20

---
+++ IIum_Medioz/app/src/main/java/com/iium/iium_medioz/view/main/bottom/data/DataModifyActivity.kt
@@ -0,0 +1,32 @@
+package com.iium.iium_medioz.view.main.bottom.data
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
+import com.iium.iium_medioz.R
+import com.iium.iium_medioz.api.APIService
+import com.iium.iium_medioz.api.ApiUtils
+import com.iium.iium_medioz.databinding.ActivityDataModifyBinding
+import com.iium.iium_medioz.databinding.ActivitySaveBinding
+import com.iium.iium_medioz.util.base.BaseActivity
+
+class DataModifyActivity : BaseActivity() {
+
+    private lateinit var mBinding : ActivityDataModifyBinding
+    private lateinit var apiServices: APIService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_data_modify)
+        mBinding.activity = this
+        apiServices = ApiUtils.apiService
+        mBinding.lifecycleOwner = this
+        inStatusBar()
+    }
+
+    private fun inStatusBar() {
+        setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+        window.statusBarColor = getColor(R.color.colorPrimary)
+    }
+}
